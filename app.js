d454dbdcf8bfcf18463d19d5bbcee6065eab9eef
Eber <doneber98@gmail.com>
2019-09-26 14:48:49 -0300

    Se configuro las rutas, vistas, etc

--- app.js
+++ app.js
@@ -2,30 +2,29 @@ import express from 'express'
 import cors from 'cors'
 import pug from 'pug'
 import morgan from 'morgan'
-import path from 'path'
 import favicon from 'serve-favicon'
+import routes from './routes/index'
 
+const app = express(),
+    port = process.env.port || 3000,
+    pathViews = `${__dirname}/views`,
+    pathFavicon = `${__dirname}/public/img/favicon.ico`,
+    pathPublic = `${__dirname}/public`;
 
-const app = express();
-const port = process.env.port || 3000;
 app
     /* configuraciones de la App */
-    .set('views', path.join(__dirname, 'views'))
+    .set('views', pathViews)
     .set('view engine', 'pug')
     .set('port', port)
     /* middlewares */
-    .use(favicon(path.join(__dirname, '/public/img/favicon.ico')))
     .use(morgan('tiny'))    //para ver las peticiones a nuestro servidor
     .use(cors())    //para hacer peticiones a otros dominios
     .use(express.json())    //para usar json en respuestas
     .use(express.urlencoded({ extended: true }))   //para trabajar con aplicaiones www-form-urlencode
-
-    .use(express.static(path.join(__dirname, 'public')))    //definimos la carpeta estatica (public)
-
+    .use(favicon(pathFavicon))
+    .use(express.static(pathPublic))    //definimos la carpeta estatica (public)
     /* rutas */
-    .get('/', (req, res) => {
-        res.render('./index.html')
-    })
+    .use('/', routes)
     .listen(port, () => {
         console.log(`Corriendo el el puerto: ${port}`)
     })
